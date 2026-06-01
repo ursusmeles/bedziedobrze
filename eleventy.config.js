9c3a254ed7967850d222134c36f5538a0ccdf56a
ursusmeles <mateusz.d.nowak@gmail.com>
2026-06-01 20:31:57 +0200

    untrack ignored directories

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+export default function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+}
